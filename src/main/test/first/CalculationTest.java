d0a7b7a4fe7c90d24dce7e1a568511aca8ab4d4d
atepaevm <atepaev_mihail@mail.ru>
2018-03-24 12:16:09 +0300

    add new tests in First

--- src/main/test/first/CalculationTest.java
+++ src/main/test/first/CalculationTest.java
@@ -18,53 +18,113 @@ public class CalculationTest {
         assertEquals(Math.asin(X+deltaX),calc.arcsin(X+deltaX),deltaY);
     }
     @Test
-    public void testFirst() throws Exception{
+    public void testFirst1() throws Exception{
         //(-oo;-1)
         testPoint(-10.12);
+    }
+    @Test
+    public void testFirst2() throws Exception{
+        //(-oo;-1)
         testPoint(-123.32342);
+    }
+    @Test
+    public void testFirst3() throws Exception{
+        //(-oo;-1)
         testPoint(-29885687);
     }
     @Test
-    public void testSecond() throws Exception{
+    public void testSecond1() throws Exception{
         //(-1;-0.5)
         testPoint(-0.99);
+    }
+    @Test
+    public void testSecond2() throws Exception{
+        //(-1;-0.5)
         testPoint(-0.74);
+    }
+    @Test
+    public void testSecond3() throws Exception{
+        //(-1;-0.5)
         testPoint(-0.58);
     }
     @Test
-    public void testThird() throws Exception{
+    public void testThird1() throws Exception{
         //(-0.5;0)
         testPoint(-0.43);
+    }
+    @Test
+    public void testThird2() throws Exception{
+        //(-0.5;0)
         testPoint(-0.25);
+    }
+    @Test
+    public void testThird3() throws Exception{
+        //(-0.5;0)
         testPoint(-0.05);
     }
     @Test
-    public void testFourth() throws Exception{
+    public void testFourth1() throws Exception{
         //(0;0.5)
         testPoint(0.14);
+    }
+    @Test
+    public void testFourth2() throws Exception{
+        //(0;0.5)
         testPoint(0.27);
+    }
+    @Test
+    public void testFourth3() throws Exception{
+        //(0;0.5)
         testPoint(0.47);
     }
     @Test
-    public void testFifth() throws Exception{
+    public void testFifth1() throws Exception{
         //(0.5;1)
         testPoint(0.7);
+    }
+    @Test
+    public void testFifth2() throws Exception{
+        //(0.5;1)
         testPoint(0.81);
+    }
+    @Test
+    public void testFifth3() throws Exception{
+        //(0.5;1)
         testPoint(0.98);
     }
     @Test
-    public void testSixth() throws Exception{
+    public void testSixth1() throws Exception{
         //(1;+oo)
         testPoint(+124333.2);
+    }
+    @Test
+    public void testSixth2() throws Exception{
+        //(1;+oo)
         testPoint(26582.954);
+    }
+    @Test
+    public void testSixth3() throws Exception{
+        //(1;+oo)
         testPoint(1.2323);
     }
     @Test
-    public void testExtraPoints() throws Exception {
+    public void testMinusOne() throws Exception {
         testPoint(-1);
+    }
+    @Test
+    public void testMinusHalf() throws Exception {
         testPoint(-0.5);
+    }
+    @Test
+    public void testZero() throws Exception {
         testPoint(0);
+    }
+    @Test
+    public void testPlusHalf() throws Exception {
         testPoint(0.5);
+    }
+    @Test
+    public void testPlusOne() throws Exception {
         testPoint(1);
     }
 }
